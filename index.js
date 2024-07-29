306058961db4718563a971afa1acc43977ffa4ea
Farjana73 <faakther73@gmail.com>
2024-07-29 15:59:34 +0600

    new project

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 
-let acc = document.querySelectorAll(".accordion");
-let arr = Array.from(acc)
-let i
-
-arr.map((item) =>{
- item.addEventListener("click", function (){
-    item.classList.toggle("active")
-   let panel = item.nextElementSibling;
-   if (panel.style.display === "block") {
-    panel.style.display = "none";
-  } else {
-    panel.style.display = "block";
-    panel.style.color = "blue"
-
-  }
- })
-})
-
 
   
