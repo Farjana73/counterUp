8d27be6334857ce9d9e6eefbd893518a8a5d4fe0
Farjana73 <faakther73@gmail.com>
2024-07-30 11:39:34 +0600

    added new file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function openCity(e, name){
+  let i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".tabcontent");
+  let myArr = Array.from(tabcontent)
 
+  myArr.map((item) =>{
+
+    console.log(item)
+  })
+
+
+}
 
   
